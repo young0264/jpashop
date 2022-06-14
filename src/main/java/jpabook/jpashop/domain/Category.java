bf724616b3d48085bc6afc6fb8ce25e58ddf2ce4
youngGit <ny2485@naver.com>
2022-06-14 16:39:39 +0900

    쇼핑사이트_핵심_도메인개발 엔티티설계_유의_h2db_인메모리

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,26 +8,27 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter
 @Setter
 public class Category {
     @Id
     @GeneratedValue
-    @Column(name = "category id")
+    @Column(name = "category_id")
     private Long id;
 
     private String name;
 
     @ManyToMany
-    @JoinTable(name = "category item",
+    @JoinTable(name = "category_item",
             joinColumns = @JoinColumn(name = "category_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id"))//중간테이블 매핑
 
-
     private List<Item> items = new ArrayList<>();
 
-    @ManyToMany
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
@@ -35,6 +36,11 @@ public class Category {
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 
 }
 
